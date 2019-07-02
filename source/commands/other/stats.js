9abbb7d6b10f191319edce7910cf50bbf65b2e7d
x-pulse <kuchurov03@mail.ru>
2019-07-02 13:42:41 +0000

    upd

--- source/commands/other/stats.js
+++ source/commands/other/stats.js
@@ -2,13 +2,12 @@ const prototypes = require('../../util/prototypes');
 const Discord = require('discord.js');
 const shell = require("executive");
 const moment = require('moment');
+const os = require('os')
 const cpuStats = require('cpu-stats');
 const memory = require("memory");
 moment.locale('ru');
 const execute = async (command) => {
-    shell(arg, (e, r, b) => {
-        return r;
-    });
+    return shell(command)
 }
 
 module.exports = {
@@ -17,24 +16,49 @@ module.exports = {
     aliases: [],
     public: true,
     async execute(client, message, args, config, settings) {
-        let uptimeAverage = execute('uptime');
-        let user = execute('whoami');
-        let nodeV = execute('node -v');
-        let npmV = execute('npm -v');
-        let allowMemory = (os.totalmem() / 1024 / 1024).toFixed(2);
-        let uptime = moment(client.uptime).fromNow();
-        let used = process.memoryUsage();
-        let embed = new Discord.RichEmbed()
-        .setColor(config.i)
-        .setDescription(`
-        Aspire world v2.0.0, discord.js \`${Discord.version}\`, \`NodeJs ${nodeV}, NPM ${npmV} \` на \`${process.platform()} ${os.release()}\`
-        Ядро загружено ${uptime}, модули загружены ${uptime}, команды загружены ${uptime}.
-        
-        Использует ${memory()} MB памяти из ${allowMemory} MB возможной, память RSS занимает ${Math.round(used['rss'] / 1024 / 1024 * 100) /100} MB, внешняя память состовляет ${Math.round(used['external'] / 1024 / 1024 * 100) /100} MB
-        Запущен на процессе PID ${process.pid} (\`NodeJs ${nodeV}\`)
 
-        Данный бот не использует осколки процессов, и может видить ${client.guilds.size} серверов с ${client.users.size} пользователями
-        Средняя задержка веб-сокета: ${Number(client.ping).toFixed(2)}ms
+        var exec = require('child_process').exec;
+
+        let execPromise = (cmd) => {
+            return new Promise(function (resolve, reject) {
+                exec(cmd, function (err, stdout) {
+                    if (err) return reject(err);
+                    resolve(stdout);
+                });
+            });
+        }
+
+
+        let uptimeAverage = await execPromise('uptime');
+        let user = await execPromise('whoami');
+        let nodeV = await execPromise('node -v');
+        let npmV = await execPromise('npm -v');
+        let allowMemory = (os.totalmem() / 1024 / 1024).toFixed(2);
+        let uptime = moment(process.uptime()).fromNow();
+        let used = process.memoryUsage();
+        let i = 0;
+        let loaded;
+        await cpuStats(1000, async function (error, result) {
+            loaded = `${result.map(x => `#${++i} Процессор ::
+Loaded_for ${x.cpu | 0}%
+Of_which ${x.sys | 0}% is allocated for the system
+IDLE_for ${x.idle | 0}%\n`)}
+`
+            await message.channel.send(`
+Aspire world v2.0.0, discord.js \`${Discord.version}\`, (\`NodeJs ${nodeV}\`) \`Npm ${npmV} \` на \`${process.platform} ${os.release()}\`
+Ядро загружено ${uptime}, модули загружены ${uptime}, команды загружены ${uptime}.
+        
+RSS Использует ${memory()} MB памяти из ${allowMemory} MB возможной, основная память занимает ${Math.round(used['heapUsed'] / 1024 / 1024 * 100) / 100} MB, внешняя память составляет ${Math.round(used['external'] / 1024 / 1024 * 100) / 100} MB
+Запущен на процессе PID ${process.pid} (\`NodeJs ${nodeV}\`)
+
+Данный бот не использует осколки процессов, и может видить ${client.guilds.size} серверов с ${client.users.size} пользователями
+Средняя задержка веб-сокета: ${Number(client.ping).toFixed(2)}ms
+Информация сервера, от имени пользователя ${user}
+\`${uptimeAverage}\`
+
+Загруженность сервера:
+\`\`\`prolog\n${loaded}\`\`\`
         `)
+        })
     }
 }
